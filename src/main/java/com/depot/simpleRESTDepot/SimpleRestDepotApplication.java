cf99716b1d4c5f385e379978fa2fb06e011cfc13
MaciejG78 <Maciej_G@poczta.fm>
2020-02-11 15:34:01 +0100

    Pierwszy commit

---
+++ src/main/java/com/depot/simpleRESTDepot/SimpleRestDepotApplication.java
@@ -0,0 +1,13 @@
+package com.depot.simpleRESTDepot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestDepotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRestDepotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/depot/simpleRESTDepot/SimpleRestDepotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depot.simpleRESTDepot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRestDepotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
